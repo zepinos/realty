e5be701146b614c028e5ef1ad1a8b98f3489026e
zepinos <zepinos@gmail.com>
2019-04-15 16:04:51 +0900

    Spring Initialize

---
+++ src/main/java/com/zepinos/realty/RealtyApplication.java
@@ -0,0 +1,13 @@
+package com.zepinos.realty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zepinos/realty/RealtyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zepinos.realty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealtyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
