9b1039cef1f4e2dd749360829e5ceeccd84570c3
zepinos <zepinos@gmail.com>
2019-04-17 13:51:49 +0900

    JOOQ POJO 에 매개변수 전달하는 것까지 구현.

--- src/main/java/com/zepinos/realty/configuration/WebSecurityConfig.java
+++ src/main/java/com/zepinos/realty/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.zepinos.realty.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,7 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+//        http.httpBasic().disable();
+
+        http.cors().and().csrf().disable();
 
         http.authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
--- src/main/java/com/zepinos/realty/controller/RealtyController.java
+++ src/main/java/com/zepinos/realty/controller/RealtyController.java
@@ -1,9 +1,13 @@
 package com.zepinos.realty.controller;
 
+import com.zepinos.realty.jooq.tables.pojos.RealtyList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -13,6 +17,19 @@ public class RealtyController {
     @GetMapping("/new")
     public String newRealty() {
         return "realty/new";
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    public Map<String, Object> post(@ModelAttribute RealtyList realtyList) {
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", 0);
+
+        log.info("realtyList : {}", realtyList);
+
+        return result;
+
     }
 
 }
