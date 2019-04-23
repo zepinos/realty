216d4cdaa6d59c3eda1265e7460dfb4a195ff03b
zepinos <zepinos@gmail.com>
2019-04-23 16:37:49 +0900

    우편번호 찾기 라이브러리 주소 변경.
    Spring Security 인증 설정.

--- src/main/java/com/zepinos/realty/configuration/WebSecurityConfig.java
+++ src/main/java/com/zepinos/realty/configuration/WebSecurityConfig.java
@@ -1,19 +1,34 @@
 package com.zepinos.realty.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
+    //    @Override
 //    public void configure(WebSecurity web) throws Exception {
 //        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
 //    }
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder());
+
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,18 +39,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/img/**", "/vendor/**").permitAll()
-//                .antMatchers("/admin/**").hasRole("ROLE_ADMIN")
-                .antMatchers("/realty/**").permitAll()
+                .antMatchers("/realty/**").hasAuthority("ROLE_USER")
+                .antMatchers("/group/**").hasAuthority("ROLE_GROUP")
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated();
 
-//        http.formLogin()
+        http.formLogin()
 //                .loginPage("/login") // default
 //                .loginProcessingUrl("/authenticate")
 //                .failureUrl("/login?error") // default
-//                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/realty")
 //                .usernameParameter("email")
 //                .passwordParameter("password")
-//                .permitAll();
+//                .permitAll()
+        ;
 //
 //        http.logout()
 //                .logoutUrl("/logout") // default
